03dcd9c6b4b43ec51bc2960c641bd98134293653
hecrw <136959713+hecrw@users.noreply.github.com>
2023-08-30 11:29:34 +0300

    added suggested corrections made by chatgpt
    i couldnt be bothered to implement this ngl

--- Spell checker/Trie.py
+++ Spell checker/Trie.py
@@ -27,6 +27,22 @@ class Trie:
             node = node.children[c]
         return node.is_word
 
+    def suggest_corrections(self, word, max_suggestions=5):
+        suggestions = []
+        self._dfs_corrections(self.root, word, "", suggestions, max_suggestions)
+        return suggestions
+
+    def _dfs_corrections(self, node, current_word, current_path, suggestions, max_suggestions):
+        if node.is_word and current_path != current_word:
+            similarity = self._calculate_similarity(current_word, current_path)
+            heapq.heappush(suggestions, (similarity, current_path))
+
+            if len(suggestions) > max_suggestions:
+                heapq.heappop(suggestions)
+
+        for char, child_node in node.children.items():
+            self._dfs_corrections(child_node, current_word, current_path + char, suggestions, max_suggestions) 
+
 english_words = words.words()
 dictionary = Trie()
 for word in english_words:
