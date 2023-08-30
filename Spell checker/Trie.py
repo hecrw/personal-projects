059d98c0244fe0eb9cb082148db258b6d4b3a8c4
hecrw <136959713+hecrw@users.noreply.github.com>
2023-08-30 11:36:32 +0300

    Update Trie.py

--- Spell checker/Trie.py
+++ Spell checker/Trie.py
@@ -42,7 +42,28 @@ class Trie:
                 heapq.heappop(suggestions)
 
         for char, child_node in node.children.items():
-            self._dfs_corrections(child_node, current_word, current_path + char, suggestions, max_suggestions) 
+            self._dfs_corrections(child_node, current_word, current_path + char, suggestions, max_suggestions)
+
+    def _calculate_similarity(word1, word2):
+        m, n = len(word1), len(word2)
+        dp = [[0] * (n + 1) for _ in range(m + 1)]
+    
+        for i in range(m + 1):
+            dp[i][0] = i
+    
+        for j in range(n + 1):
+            dp[0][j] = j
+    
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+                cost = 0 if word1[i - 1] == word2[j - 1] else 1
+                dp[i][j] = min(
+                    dp[i - 1][j] + 1,
+                    dp[i][j - 1] + 1,
+                    dp[i - 1][j - 1] + cost
+                )
+    
+        return dp[m][n]
 
 english_words = words.words()
 dictionary = Trie()
