2888b262af438438c032583abb0935e0a4765502
hecrw <136959713+hecrw@users.noreply.github.com>
2023-08-31 17:04:58 +0300

    yippie

--- Spell checker/gui.py
+++ Spell checker/gui.py
@@ -1,37 +1,52 @@
 import re
 import tkinter as tk
+import ttkbootstrap as ttk
+from tkinter.font import Font
 from tkinter.scrolledtext import ScrolledText
 from Trie import dictionary
 
 class spellingchecker:
     
     def __init__(self):
-        self.window = tk.Tk()
-        self.window.geometry("600x500")
-        self.text = ScrolledText(self.window, font=("Ariel", 14))
-        self.text.bind("<KeyRelease>", self.check)
-        self.text.pack()
         self.spaces = 0
+        #window
+        self.window = ttk.Window(themename='darkly')
+        self.window.geometry("1000x700")
+        
+        
+        #text box buttons and frames
+        self.frame = ttk.Frame(self.window)
+        self.buttons = ttk.Frame(self.frame)
+        self.label = ttk.Label(self.buttons, text="Suggested words:")
+        self.text = ScrolledText(self.frame, font=("Ariel", 12), width=60, height=20)
+        self.text.bind("<KeyRelease>", self.check)
+        
+        
+        #packs
+        self.text.pack(side="left")
+        self.label.pack()
+        self.buttons.pack(side="left", fill="both", expand=True, padx=10, pady=(0, 10))
+        self.frame.pack()
         self.window.mainloop()
         
     def check(self, event):
-        content = self.text.get("1.0", tk.END)
-        space_count = content.count(" ")
-        
-        if space_count != self.spaces:
-            self.spaces = space_count
+        content = self.text.get("1.0", "end-1c")
+        curr = content.count(" ")
+        if curr != self.spaces:
+            self.spaces = curr
             
-            for tag in self.text.tag_names():
-                self.text.tag_delete(tag)
+        for tag in self.text.tag_names():
+            self.text.tag_delete(tag)
             
-            for word in content.split(" "):
-                t = re.sub(r"[\W]", '', word.lower())
-                if not dictionary.search(t):
-                    position = content.index(word)
-                    tag_name = f"misspelled_{position}"
-                    self.text.tag_add(tag_name, f"1.{position}", f"1.{position + len(word)}")
-                    self.text.tag_config(tag_name, foreground="red", underline=True)
-                    self.text.tag_bind(tag_name, "<Button-1>", lambda event, w=word: self.show_suggestions(event, w))
+        for word in content.split(" "):
+            temp = re.sub(r'[^\w\s]', '', word)
+            
+            if not dictionary.search(temp):
+                pos = content.index(word)
+                tag_name = f"misspelled_{pos}"
+                self.text.tag_add(tag_name, f"1.{pos}", f"1.{pos + len(word)}")
+                self.text.tag_config(tag_name, underline=True, foreground="red")
+                self.text.tag_bind(tag_name, "<Button-1>", lambda event, w = word : self.show_suggestions(event, w))
         
     def show_suggestions(self, event, misspelled_word):
         suggestions = dictionary.suggestions(misspelled_word)
