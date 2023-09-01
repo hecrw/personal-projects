66c7f001a1366f39c78688be4d6cd08a9f05b744
hecrw <136959713+hecrw@users.noreply.github.com>
2023-09-01 22:59:30 +0300

    Update yippie.py

--- type tester/yippie.py
+++ type tester/yippie.py
@@ -8,34 +8,47 @@ class type_tester:
     
     def __init__(self):
         #assignments
-        self.spaces = 0
         self.time = 15
         with open('words.txt', 'r') as file:
             self.common_words = file.read().splitlines()
-        self.common_words = random.sample(self.common_words, 350)
+        self.common_words = random.sample(self.common_words, 400)
+        self.portion = self.common_words[0: 10] 
+        self.portion = " ".join(self.portion)
         
         #widgets
         self.window = tk.Tk()
         self.widgets = ttk.Frame(self.window)
         self.buttons = ttk.Frame(self.widgets)
         self.window.geometry("1000x700")
-        self.label = ttk.Label(self.window, font=("Ariel", 14))
+        self.label = ttk.Label(self.window, font=("Ariel", 14), text=self.portion)
         self.text = ScrolledText(self.widgets, width=50,height=5, font=("Ariel", 14))
         self.first = ttk.Button(self.buttons, text="15s", command=self.set_time(0))
         self.second = ttk.Button(self.buttons, text="30s", command=self.set_time(1))
         self.third = ttk.Button(self.buttons, text="60s", command=self.set_time(2))
         
+        #bind
+        self.text.bind("<KeyRelease>", self.check)
+        
         #pack
+        self.label.pack(pady=10)
         self.buttons.pack(side="left")
         self.widgets.pack()
         self.first.pack(); self.second.pack(); self.third.pack()
         self.text.pack()
         self.window.mainloop()
         
-        self.text.bind("<keyRelease>", self.check)
         
     def check(self, event):
-        pass
+        content = self.text.get("1.0", tk.END).split()
+        curr = content.count(" ")
+        size = len(self.portion.split())
+        if size <= len(content):
+            self.portion = self.common_words[size:size+10]
+            self.portion = " ".join(self.portion)
+            self.label.config(text=self.portion)
+        
+        
+        
     
     def set_time(self, idx):
         lst = [15, 30, 60]
